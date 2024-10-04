bca21be410194000547a2c9772412cfd8ba94de8
JohanHammerin <Johan04hammerin@gmail.com>
2024-10-04 18:44:54 +0200

    18:44

---
+++ src/main/java/se/johan_hammerin/w3Schools/p_Date/Uppgift_1/Main.java
@@ -0,0 +1,9 @@
+package se.johan_hammerin.w3Schools.p_Date.Uppgift_1;
+import java.time.LocalDate;
+public class Main {
+    public static void main(String[] args) {
+        LocalDate todaysDate = LocalDate.now();
+        System.out.println(todaysDate);
+    }
+
+}
